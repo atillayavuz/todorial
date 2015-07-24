00933e40a59e92c504ab3e689cf8bcd1050efd22
atilla yavuz <atillayavuz@gmail.com>
2015-07-24 23:27:52 +0300

    Creates service classes

---
+++ Todorial/Todorial.BusinessLogic/Common/IEntityService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Todorial.Data.Domain.Abstract;
+
+namespace Todorial.BusinessLogic.Common
+{
+    public interface IEntityService<T>
+     where T : BaseEntity
+    {
+        void Create(T entity);
+        void Delete(T entity); 
+        IEnumerable<T> GetAll();
+        void Update(T entity);
+    }
+}
---
+++ Todorial/Todorial.BusinessLogic/Service/Interfaces/ITodoItemService.cs
@@ -0,0 +1,10 @@
+﻿using Todorial.BusinessLogic.Common;
+using Todorial.Data.Domain;
+
+namespace Todorial.BusinessLogic.Service.Interfaces
+{
+    public interface ITodoItemService : IEntityService<TodoItem>
+    {
+
+    }
+}
