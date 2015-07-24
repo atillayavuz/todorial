7d0412a14c001a9564ebdc73a9768014d18c737b
atilla yavuz <atillayavuz@gmail.com>
2015-07-24 23:41:04 +0300

    Creates custom-exception-handling-for-webapi

---
+++ Todorial/Todorial.API/CustomFilter/ProcessExceptionFilterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Web.Http.Filters;
+
+namespace Todorial.API.CustomFilter
+{
+    public class ProcessExceptionFilterAttribute : ExceptionFilterAttribute, IExceptionFilter
+    {
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        { 
+
+            if (actionExecutedContext.Exception is ProcessException)
+            { 
+                var res = actionExecutedContext.Exception.Message;
+                 
+                HttpResponseMessage response = new HttpResponseMessage(System.Net.HttpStatusCode.InternalServerError)
+                {
+                    Content = new StringContent(res),
+                    ReasonPhrase = res
+                };
+                 
+                actionExecutedContext.Response = response;
+            }
+        }
+    }
+}
